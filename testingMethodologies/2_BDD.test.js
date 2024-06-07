0b6ec62b455db6132e808b107a7cb5ac6d9eeeb0
Rajasekar <rajasekark54@gmail.com>
2024-06-07 16:04:15 +0530

    Added examples to illustrate the Behavior Driven Development(BDD)

---
+++ testingMethodologies/2_BDD.test.js
@@ -0,0 +1,21 @@
+/* 
+Behavior-Driven Development (BDD)
+  Definition: BDD is an extension of TDD that encourages collaboration between developers, QA, and non-technical or business participants. It emphasizes examples to clarify requirements and tests.
+*/
+
+const BookStore = require('../code/bookStore');
+
+describe('BookStore', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new BookStore();
+  });
+
+  test('should add a book to the store', () => {
+    store.addBook({ title: 'Clean Code', author: 'Robert C. Martin' });
+    expect(store.getBooks()).toEqual(
+      expect.arrayContaining([expect.objectContaining({ title: 'Clean Code' })])
+    );
+  });
+});
